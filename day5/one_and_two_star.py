89e950c6f963c094262575ced14d3deac9a372c8
Michał Walewski <mk.walewski@gmail.com>
2022-12-06 20:03:26 +0100

    Day 5 - change input

--- day5/one_and_two_star.py
+++ day5/one_and_two_star.py
@@ -1,33 +1,37 @@
 import re
+import math
 from pprint import pprint
 
 result = ''
 two_star = True
-boxes_small_input = [
-    ['Z', 'N'],
-    ['M', 'C', 'D'],
-    ['P']
-]
-boxes_input = [
-    ['B', 'W', 'N'],
-    ['L', 'Z', 'S', 'P', 'T', 'D', 'M', 'B'],
-    ['Q', 'H', 'Z', 'W', 'R'],
-    ['W', 'D', 'V', 'J', 'Z', 'R'],
-    ['S', 'H', 'M', 'B'],
-    ['L', 'G', 'N', 'J', 'H', 'V', 'P', 'B'],
-    ['J', 'Q', 'Z', 'F', 'H', 'D', 'L', 'S'],
-    ['W', 'S', 'F', 'J', 'G', 'Q', 'B'],
-    ['Z', 'W', 'M', 'S', 'C', 'D', 'J']
-]
+boxes = []
+boxesDict = {}
 
 with open('input.txt') as file:
-    if 'sample' in file.name:
-        boxes = boxes_small_input
-    else:
-        boxes = boxes_input
     for line in file:
         string = line.rstrip()
-        # finds = re.findall(r"(\s*)\[(\w+)\]", string)
+        finds = re.findall(r"(\s*)\[(\w+)\]", string)
+        if finds:
+            row = 0
+            total_rest = 0
+            for find in finds:
+                row += 1
+                if row == 1:
+                    spaces = len(find[0])
+                else:
+                    spaces = len(find[0]) - 1
+                curr_rest = math.floor(spaces / 4)
+                total_rest += curr_rest
+                index = (row - 1) + total_rest
+                for i in range(0, index + 1):
+                    if i not in boxesDict:
+                        boxesDict[i] = []
+                boxesDict[index].append(find[1])
+        if not string:
+            for i in boxesDict:
+                boxArray = boxesDict[i]
+                boxArray.reverse()
+                boxes.append(boxArray)
         finds = re.findall(r"move (\d+) from (\d+) to (\d+)", string)
         if finds:
             how_many = int(finds[0][0])
